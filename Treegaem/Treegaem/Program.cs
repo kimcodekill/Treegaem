69460c08450b96d0b5479c81e4442076d44a997b
unknown <ruben.andersson98@gmail.com>
2017-09-14 17:11:18 +0200

    Added list of trees

--- Treegaem/Treegaem/Program.cs
+++ Treegaem/Treegaem/Program.cs
@@ -10,6 +10,106 @@ namespace Treegaem
     {
         static void Main(string[] args)
         {
+            Random r = new Random();
+            int money;
+            int difficulty;
+            bool done = false;
+
+            do
+            {
+                Console.WriteLine("Choose difficulty: ");
+                Console.WriteLine("1. Easy");
+                Console.WriteLine("2. Medium");
+                Console.WriteLine("3. Hard");
+                difficulty = int.Parse(Console.ReadLine());
+
+                if (difficulty == 1)
+                {
+                    money = 5000;
+                    done = true;
+                }
+                else if (difficulty == 2)
+                {
+                    money = 2000;
+                    done = true;
+                }
+                else if (difficulty == 3)
+                {
+                    money = 500;
+                    done = true;
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("{0} is not a valid input", difficulty));
+                }
+            }
+            while (!done);
+            List<Tree> TreesDB = new List<Tree>();
+
+            TreesDB.Add(new Tree()
+            {
+                TreeType = "Apple",
+                Growth = 0.1,
+                Price = 250,
+                FruitGrowth =0.3,
+                FruitDecay =1.2
+            });
+            TreesDB.Add(new Tree()
+            {
+                TreeType = "Pear",
+                Growth = 0.15,
+                Price = 350,
+                FruitGrowth = 0.25,
+                FruitDecay = 1.4
+            });
+            TreesDB.Add(new Tree()
+            {
+                TreeType = "Orange",
+                Growth = 0.05,
+                Price = 600,
+                FruitGrowth = 0.15,
+                FruitDecay = 1.5
+            });
+            TreesDB.Add(new Tree()
+            {
+                TreeType = "Banana",
+                Growth = 0.5,
+                Price = 750,
+                FruitGrowth = 0.2,
+                FruitDecay = 1.3
+            });
+            TreesDB.Add(new Tree()
+            {
+                TreeType = "Peach",
+                Growth = 0.25,
+                Price = 500,
+                FruitGrowth = 0.6,
+                FruitDecay = 1.5
+            });
+            TreesDB.Add(new Tree()
+            {
+                TreeType = "Cherry",
+                Growth = 0.05,
+                Price = 500,
+                FruitGrowth = 0.95,
+                FruitDecay = 1.65
+            });
+            TreesDB.Add(new Tree()
+            {
+                TreeType = "Pine",
+                Growth = 0.6,
+                Price = 150,
+                FruitGrowth = 0.05,
+                FruitDecay = 1.01
+            });
+            TreesDB.Add(new Tree()
+            {
+                TreeType = "Oak",
+                Growth = 0.05,
+                Price = 1000,
+                FruitGrowth = 0.05,
+                FruitDecay = 1.01
+            });
         }
     }
 }
--- Treegaem/Treegaem/Tree.cs
+++ Treegaem/Treegaem/Tree.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Treegaem
 {
-    class Tree
+    public class Tree
     {
+        public int Height { get; set; }
+
+        public string FruitType { get; set; }
+
+        public int FruitAmount { get; set; }
+
+        public string TreeType { get; set; }
+
+        public int Quality { get; set; }
+
+        public int Price { get; set; }
+
+        public double Growth { get; set; }
+
+        public int FruitGrowth { get; set; }
+
+        public int FruitDecay { get; set; }
     }
 }
