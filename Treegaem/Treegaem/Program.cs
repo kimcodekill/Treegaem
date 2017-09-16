b752ad5e55c29b750075baed53084ed24fe247cb
unknown <ruben.andersson98@gmail.com>
2017-09-16 17:16:30 +0200

    Added method for wrong inputs and added printing of trees

--- Treegaem/Treegaem/Program.cs
+++ Treegaem/Treegaem/Program.cs
@@ -19,6 +19,14 @@ namespace Treegaem
             int buyTree;
             bool done = false;
             int menu = 0;
+            int apple = 0;
+            int pear = 0;
+            int orange = 0;
+            int banana = 0;
+            int peach = 0;
+            int cherry = 0;
+            int pine = 0;
+            int oak = 0;
 
             //Counters for things
             int c = 1;
@@ -44,7 +52,7 @@ namespace Treegaem
             });
             TreesDB.Add(new Tree()
             {
-                TreeType = "Orange",    
+                TreeType = "Orange",
                 Growth = 0.05,
                 Price = 600,
                 FruitGrowth = 0.15,
@@ -58,7 +66,7 @@ namespace Treegaem
                 Price = 750,
                 FruitGrowth = 0.2,
                 FruitDecay = 1.3,
-                Height = 1.5                
+                Height = 1.5
             });
             TreesDB.Add(new Tree()
             {
@@ -67,7 +75,7 @@ namespace Treegaem
                 Price = 500,
                 FruitGrowth = 0.6,
                 FruitDecay = 1.5,
-                Height= 0.5
+                Height = 0.5
             });
             TreesDB.Add(new Tree()
             {
@@ -128,13 +136,13 @@ namespace Treegaem
                         break;
 
                     default:
-                        Console.WriteLine(string.Format("{0} is not a valid input", difficulty));
+                        WrongInput(difficulty);
                         break;
                 }
             }
             while (!done);
 
-            
+
 
             Console.Clear();
             //Menu
@@ -158,63 +166,72 @@ namespace Treegaem
                             Console.WriteLine(string.Format("{0}. {1}, {2} moneys", c, t.TreeType, t.Price));
                             c++;
                         }
-
+                        c = 1;
                         Console.WriteLine("What tree do you want to buy?");
                         buyTree = int.Parse(Console.ReadLine());
-
-
+                        Console.WriteLine();
 
                         //EXTEND SWITCH IF YOU ADD MORE TREES
                         switch (buyTree)
                         {
                             case 1:
                                 Trees.Add(TreesDB[1]);
+                                money -= TreesDB[1].Price;
                                 break;
                             case 2:
                                 Trees.Add(TreesDB[2]);
+                                money -= TreesDB[2].Price;
                                 break;
                             case 3:
                                 Trees.Add(TreesDB[3]);
+                                money -= TreesDB[3].Price;
                                 break;
                             case 4:
                                 Trees.Add(TreesDB[4]);
+                                money -= TreesDB[4].Price;
                                 break;
                             case 5:
                                 Trees.Add(TreesDB[5]);
+                                money -= TreesDB[5].Price;
                                 break;
                             case 6:
                                 Trees.Add(TreesDB[6]);
+                                money -= TreesDB[6].Price;
                                 break;
                             case 7:
                                 Trees.Add(TreesDB[7]);
+                                money -= TreesDB[7].Price;
                                 break;
                             case 8:
                                 Trees.Add(TreesDB[8]);
+                                money -= TreesDB[8].Price;
                                 break;
                             default:
-                                Console.WriteLine(string.Format("{0} is not a valid input", buyTree));
+                                WrongInput(buyTree);
                                 break;
 
                         }
-
-
-
-
-
-
-
                         break;
+                    case 2:
+                        foreach (Tree t in Trees)
+                        {
+                            int x = Trees.Count(y => y.TreeType == t.TreeType);
+                            Console.WriteLine("{0} {1}-trees", x, t.TreeType);
+                        }
+                        break;
+
+
+
 
                     case 0:
                         done = true;
                         Environment.Exit(0);
                         break;
                     default:
-                        Console.WriteLine("Invalid input, {0}", menu);
+                        WrongInput(menu);
                         break;
                 }
 
-                
 
 
 
@@ -223,6 +240,11 @@ namespace Treegaem
 
             }
             while (menu != 0);
+        }
+
+        private static void WrongInput(int i)
+        {
+            Console.WriteLine(string.Format("{0} is not a valid input", i));
         }
     }
 }
